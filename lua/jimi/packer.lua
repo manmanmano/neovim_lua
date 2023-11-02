a9a4e558f1659c9812ebf407aa390a64e4c2480e
Mariano D'Angelo <mariano.dangelo@robinradar.com>
2023-11-02 12:37:40 +0100

    Toggleterm, buffer remaps and packer sync

--- lua/jimi/packer.lua
+++ lua/jimi/packer.lua
@@ -102,4 +102,9 @@ return require('packer').startup(function(use)
   -- copilot
   use 'github/copilot.vim'
 
+  -- toggleterm
+  use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup()
+  end}
+
 end)
