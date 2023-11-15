7be4af8a949679db00bdcda7fac4b6c878c56639
Mariano D'Angelo <mariano.dangelo@robinradar.com>
2023-11-15 13:23:55 +0100

    Edit copilot configuration

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,3 +1,11 @@
+vim.g.copilot_filetypes = {
+  markdown = true,
+  python = true,
+  lua = true,
+  yaml = true,
+  ansible = true,
+}
+
 vim.keymap.set('n', '<leader>gce', ":Copilot enable<CR>")
 vim.keymap.set('n', '<leader>gcd', ":Copilot disable<CR>")
 vim.keymap.set('n', '<leader>gcs', ":Copilot status<CR>")
