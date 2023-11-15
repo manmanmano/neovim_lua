4a58b708eb2af3ea2ead4824ce867f623e0f6172
Mariano D'Angelo <mariano.dangelo@robinradar.com>
2023-11-15 13:33:18 +0100

    Disable autocomment on new line

--- lua/jimi/init.lua
+++ lua/jimi/init.lua
@@ -4,3 +4,6 @@ require("jimi.set")
 
 --NVIM colorscheme:
 vim.cmd[[colorscheme gruvbox]]
+
+vim.cmd('autocmd BufEnter * set formatoptions-=cro')
+vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
