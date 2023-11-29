8d5a635a7e431ccbd89fae02781529d120d5a6b9
Mariano D'Angelo <mariano.dangelo@robinradar.com>
2023-11-29 19:00:35 +0100

    Change cmp mappings to use it with copilot

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -21,8 +21,8 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
