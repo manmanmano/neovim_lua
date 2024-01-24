cec35e50d3a8a3e5ad8d11ee5004e5137bfcf704
Mariano D'Angelo <mariano.dangelo@robinradar.com>
2024-01-24 14:04:32 +0100

    Remove nord

--- lua/jimi/packer.lua
+++ lua/jimi/packer.lua
@@ -13,39 +13,40 @@ return require('packer').startup(function(use)
   }
 
   -- colorschemes
-  use {
-    'andersevenrud/nordic.nvim',
-    config = function()
-        -- The table used in this example contains the default settings.
-        -- Modify or remove these to your liking (this also applies to alternatives below):
-        require('nordic').colorscheme({
-            -- Underline style used for spelling
-            -- Options: 'none', 'underline', 'undercurl'
-            underline_option = 'none',
 
-            -- Italics for certain keywords such as constructors, functions,
-            -- labels and namespaces
-            italic = true,
+  -- use {
+  --   'andersevenrud/nordic.nvim',
+  --   config = function()
+  --       -- The table used in this example contains the default settings.
+  --       -- Modify or remove these to your liking (this also applies to alternatives below):
+  --       require('nordic').colorscheme({
+  --           -- Underline style used for spelling
+  --           -- Options: 'none', 'underline', 'undercurl'
+  --           underline_option = 'none',
 
-            -- Italic styled comments
-            italic_comments = false,
+  --           -- Italics for certain keywords such as constructors, functions,
+  --           -- labels and namespaces
+  --           italic = true,
 
-            -- Minimal mode: different choice of colors for Tabs and StatusLine
-            minimal_mode = false,
+  --           -- Italic styled comments
+  --           italic_comments = false,
 
-            -- Darker backgrounds for certain sidebars, popups, etc.
-            -- Options: true, false, or a table of explicit names
-            -- Supported: terminal, qf, vista_kind, packer, nvim-tree, telescope, whichkey
-            alternate_backgrounds = false,
+  --           -- Minimal mode: different choice of colors for Tabs and StatusLine
+  --           minimal_mode = false,
 
-            -- Callback function to define custom color groups
-            -- See 'lua/nordic/colors/example.lua' for example defitions
-            custom_colors = function(c, s, cs)
-              return {}
-            end
-        })
-    end
-  }
+  --           -- Darker backgrounds for certain sidebars, popups, etc.
+  --           -- Options: true, false, or a table of explicit names
+  --           -- Supported: terminal, qf, vista_kind, packer, nvim-tree, telescope, whichkey
+  --           alternate_backgrounds = false,
+
+  --           -- Callback function to define custom color groups
+  --           -- See 'lua/nordic/colors/example.lua' for example defitions
+  --           custom_colors = function(c, s, cs)
+  --             return {}
+  --           end
+  --       })
+  --   end
+  -- }
 
   use 'sainnhe/everforest'
   use { "ellisonleao/gruvbox.nvim" }
