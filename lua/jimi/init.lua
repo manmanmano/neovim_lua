3b595ce6ed8dc456272287bd8eaa341d8f1cc4a1
Mariano D'Angelo <mariano.dangelo@robinradar.com>
2024-03-28 10:15:00 +0100

    Fix syntax highilighting for jinja templates

--- lua/jimi/init.lua
+++ lua/jimi/init.lua
@@ -8,3 +8,4 @@ vim.cmd[[colorscheme gruvbox]]
 vim.cmd('autocmd BufEnter * set formatoptions-=cro')
 vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {pattern = "Jenkinsfile", command = "set filetype=groovy"})
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {pattern = "*.j2", command = "set filetype=yaml"})
